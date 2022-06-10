a893064abc9ed313a897db80e57ec0d34b121d32
Andrey Nilender <Nilenderav@mail.ru>
2022-06-10 18:48:43 +0400

    feat: add board and cells

---
+++ chess/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
+
+const AppStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+function App() {
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
+  return (
+    <AppStyle>
+      <BoardComponent board={board} setBoard={setBoard} />
+    </AppStyle>
+  );
+}
+
+export default App;
